7d9bb79aa5af8c4de5e3bb86de807abda516dbc1
Ryan Cordial <ryancordial.s@gmail.com>
2023-12-01 22:22:00 +0800

    Update date and Booking

--- app/src/main/java/com/example/onlinelaundry/Booking/BookingActivity.java
+++ app/src/main/java/com/example/onlinelaundry/Booking/BookingActivity.java
@@ -1,8 +1,10 @@
 package com.example.onlinelaundry.Booking;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
@@ -41,8 +43,10 @@ import retrofit2.Response;
 public class BookingActivity   extends AppCompatActivity {
 
     private AppCompatButton btnSubmit, btnHome;
-    private Spinner dateSpinner, servicesSpinner;
-    private RadioButton cash, gCash, pickUp, delivery;
+    private Spinner servicesSpinner;
+    private AppCompatButton dateSpinner;
+
+    private RadioButton cash, gCash, pickUp, delivery, kg, pack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +55,10 @@ public class BookingActivity   extends AppCompatActivity {
 
         cash = findViewById(R.id.cash);
         gCash = findViewById(R.id.gcash);
+        kg = findViewById(R.id.kg);
         pickUp = findViewById(R.id.pickUp);
         delivery = findViewById(R.id.delivery);
+        kg.setChecked(true);
         cash.setChecked(true);
         pickUp.setChecked(true);
 
@@ -61,17 +67,9 @@ public class BookingActivity   extends AppCompatActivity {
 
         dateSpinner = findViewById(R.id.dateSpinner);
 
-        Calendar calendar = Calendar.getInstance();
-        Date startDate = calendar.getTime();
-
-        List<String> dateList = getDates(startDate, 31);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, dateList);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        dateSpinner.setAdapter(adapter);
-
+        dateSpinner.setOnClickListener(view -> {
+            openDateDialog(dateSpinner);
+        });
 
         servicesSpinner = findViewById(R.id.servicesSpinner);
 
@@ -89,16 +87,36 @@ public class BookingActivity   extends AppCompatActivity {
         btnSubmit.setOnClickListener(view -> {
             SharedPreferences sharedPreferences = getSharedPreferences("user_data", Context.MODE_PRIVATE);
             String accountId = sharedPreferences.getString("accountId", "");
-            String getDate = dateSpinner.getSelectedItem().toString();
+            String getDate = dateSpinner.getText().toString();
             String getService = servicesSpinner.getSelectedItem().toString();
+            String getUnit = kg.isChecked() ? "per Kg" : "per Package";
             String getPaymentMethod = cash.isChecked() ? "cash" : "gCash";
             String getPickUpOption = cash.isChecked() ? "Pick Up" : "Delivery";
 
-            submitBooking(accountId, getDate, getService, getPaymentMethod, getPickUpOption);
+            if(getDate.equals("Select Date") ){
+                Toast.makeText(this, "Please select date.", Toast.LENGTH_SHORT).show();
+            }else{
+                submitBooking(accountId, getDate, getService,getUnit, getPaymentMethod, getPickUpOption);
+            }
         });
     }
 
-    private void submitBooking(String accountId, String date, String service, String paymentMethod, String pickUpOption){
+    private void openDateDialog(AppCompatButton btn) {
+        DatePickerDialog dialog = new DatePickerDialog(this,
+                (datePicker, year, month, day) -> {
+                    String sYear = String.valueOf(year);
+                    String sMonth = String.valueOf(month + 1);
+                    String sDay = String.valueOf(day);
+
+                    btn.setText(sYear + "-" + sMonth + "-"+ sDay);
+                },
+
+                2023, 0, 0);
+
+        dialog.show();
+    }
+
+    private void submitBooking(String accountId, String date, String service, String unit, String paymentMethod, String pickUpOption){
         LoadingDialog loadingDialog = new LoadingDialog(this);
         loadingDialog.show();
 
@@ -109,8 +127,10 @@ public class BookingActivity   extends AppCompatActivity {
         jsonObject.addProperty("accountId", accountId);
         jsonObject.addProperty("date", date);
         jsonObject.addProperty("service", service);
+        jsonObject.addProperty("unit", unit);
         jsonObject.addProperty("paymentMethod", paymentMethod);
         jsonObject.addProperty("pickUpOption", pickUpOption);
+        jsonObject.addProperty("total", 0);
 
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonObject.toString());
 
@@ -150,20 +170,5 @@ public class BookingActivity   extends AppCompatActivity {
                 Toast.makeText(BookingActivity.this, "Network error. Please check your network connection.", Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    private List<String> getDates(Date startDate, int numberOfDays) {
-        List<String> dateList = new ArrayList<>();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(startDate);
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
-
-        for (int i = 0; i < numberOfDays; i++) {
-            dateList.add(dateFormat.format(calendar.getTime()));
-            calendar.add(Calendar.DAY_OF_MONTH, 1);
-        }
-
-        return dateList;
     }
 }
